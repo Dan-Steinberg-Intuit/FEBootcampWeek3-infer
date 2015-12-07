95e2e98904cfd5f4fd08678f19eab56101d4280d
Dan Steinberg <daniel_steinberg@intuit.com>
2015-12-07 13:43:45 -0800

    Lab 3.2: Add simple Backbone object with getters and setters

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,4 +1,4 @@
-require(["sample"], function(sample){
+require(["UserModel"], function(UserModel){
 	// Test that jQuery is loaded
 	$('.target').html('jQuery is working <br>');
 
@@ -11,6 +11,26 @@ require(["sample"], function(sample){
     var model = new Backbone.Model({isWorking: 'working'});
     $('.target').append('Backbone is ' + model.get('isWorking'));
 
-    // Test a sample module load
-    sample.tryMe();
+
+    var Dan = new UserModel();
+    Dan.set("name", "Dan");
+    console.log("Dan's name = " + Dan.get("name"));
+    console.log("Dan = " + JSON.stringify(Dan));
+
+    var Roger = new UserModel();
+    Roger.set({
+    	name: "Roger",
+    	account_type: "Free",
+    	signup_date: "12/31/2014",
+    	business_category: "manufacturing"
+    });
+    console.log("Roger's name = " + Roger.get("name"));
+    console.log("Roger's account type = " + Roger.get("account_type"));
+    console.log("Roger's signup date = " + Roger.get("signup_date"));
+    console.log("Roger's business category = " + Roger.get("business_category"));
+    console.log("Roger = " + JSON.stringify(Roger));
+
+    var Craig = new UserModel();
+    console.log("Craig = " + JSON.stringify(Craig));
+
 });
